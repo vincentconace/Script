a6eac22b540635c54bc2f548d5480eb4c51ea7c8
extviconace <ext_viconace@mercadolibre.com>
2021-08-04 16:41:11 -0300

    add go16 ssh

---
+++ go16.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+echo "Instalando Golang..."
+sleep 1
+echo "Feliz Viernes"
+
+
+## INICIO ##
+
+apt update
+apt-get update -y
+apt-get -y upgrade
+
+
+# Descargo golang
+
+wget https://dl.google.com/go/go1.16.6.linux-amd64.tar.gz
+tar -xvf go1.16.6.linux-amd64.tar.gz
+rm go1.16.6.linux-amd64.tar.gz
+mv go /usr/local
+
+
+# .profile
+
+echo "
+# Go
+export GOROOT=/usr/local/go
+export GOPATH=$""HOME/go
+export GOBIN=$""GOPATH/bin
+export PATH=$""PATH:$""GOBIN:$""GOROOT/bin
+" >> /home/lucas/.profile
+
+
+# Permisos 
+
+mkdir -p /home/lucas/go/src/github.com
+chmod 777 -R /home/lucas/go/src/github.com
+chmod 777 -R /home/lucas/go
+
+
+echo "\033[0;31mSE RECOMIENDA REINICIAR PARA UN CORRECTO FUNCIONAMIENTO"
+exit
