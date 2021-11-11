4c0e2a74b969ed326fbcb8e4150a48e2800a153e
Vincent Conace <62965960+vincentconace@users.noreply.github.com>
2021-11-11 11:13:01 -0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,5 @@
-echo -ne '\n' | ssh-keygen -t rsa -b 4096 -C "Email@Email.com"
+ssh-keygen -t rsa -b 4096 -C "Email@Email.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
-KEY=$(cat ~/.ssh/id_rsa.pub)
+ssh-keygen -t rsa -b 4096 -C "Email@Email.com" -N "SshKeyName" -f /home/UsuarioName/.ssh/NameSshKey
